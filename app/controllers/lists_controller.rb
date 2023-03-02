9052bca8749f48dcf1344ad3552a2e2b318d6b4f
tomochinn <122351367+tomochinn@users.noreply.github.com>
2023-03-02 06:50:01 +0000

    [Finish]3-11

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,26 +6,50 @@ class ListsController < ApplicationController
   
   # 以下を追加
   def create
-    # 1.&2. データを受け取り新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
+    # 1. データを受け取り新規登録するためのインスタンス作成
+    list =List.new(list_params)
+    # 2. データをデータベースに保存するためのsaveメソッド実行
     list.save
-    # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    # 3. フラッシュメッセージを定義し、詳細画面へリダイレクト
+    flash[ :notice] = "投稿が成功しました"
+    redirect_to list_path(list.id)
+    # @list = List.new(list_params)
+    # # 以下if文（バリデーション）
+    # if @list.save
+    #   redirect_to list_path(@list.id)
+    # else
+    #   render :new
+    # end
   end
 
   def index
+    @lists = List.all
   end
   
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
   
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
+  def destroy
+    list = List.find(params[:id]) #データ（レコード）を1件取得
+    list.destroy #データ（レコード）を削除
+    redirect_to '/lists' #投稿一覧画面へリダイレクト
+  end
+
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
+
